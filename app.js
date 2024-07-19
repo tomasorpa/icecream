e9dd4d674f6ce1d13f83b2084bcd035433103a9a
Tomas Ortega <tomasorpa@gmail.com>
2024-07-19 16:47:24 -0400

    finishing project

---
+++ app.js
@@ -0,0 +1,14 @@
+
+  let enlacesHeader = document.querySelectorAll(".div__links")[0];
+  let semaforo = true;
+
+  document.querySelectorAll("#burger")[0].addEventListener("click", function () {
+      enlacesHeader.classList.toggle("menudos")
+  })
+
+ let btnColor = document.getElementById("botonColor");
+ let tru = true;
+
+
+    
+
